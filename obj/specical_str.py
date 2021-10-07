150ea2c30eda9b77ba0756726c092024f0c7632f
Simon.Chen <chenfuyuan0713@163.com>
2021-10-08 06:35:16 +0800

    perfect specical_str

---
+++ obj/specical_str.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,name):
+        self.__name = name;
+
+    def __str__(self):
+        return "Student Object(name: %s)" % self.__name;
+    __repr__ = __str__;
+vito = Student("vito");
+print(vito);
