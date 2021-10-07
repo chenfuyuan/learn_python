0a1059360c6dd016b8a8403a76edd4fe11000531
chenfuyuan <chenfuyuan0713@163.com>
2021-10-08 07:22:07 +0800

    使用__getitem__实现一个可以用下标获取元素的斐波那契数列,支持切片

---
+++ obj/special_getitem.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'使用__getitem__实现一个可以用下标获取元素的斐波那契数列'
+
+__author__ = 'Chen'
+
+class Fib:
+
+    # getitem 参数n 可以是 整型和切片类型
+    def __getitem__(self, n):
+
+        if isinstance(n,int):
+            if n < 0:
+                raise IndexError("斐波那契数列为无限数列，无法使用负数索引")
+            a,b = 1,1;
+            for x in range(n):
+                a,b = b,a+b;
+            return a;
+
+        if isinstance(n,slice):
+            print(dir(n))
+            start = n.start;
+            stop = n.stop;
+            if start is None:
+                start = 0;
+
+            a,b = 1,1;
+            result = [];
+
+            for x in range(stop):
+                if start <= x:
+                    result.append(a);
+                a, b = b, a + b;
+            return result;
+
+
+
+fib = Fib();
+fib[-1]
+
+for i in range(0,11):
+    print(fib[i]);
+
+print("=============");
+
+fibSubList = fib[:10];
+print(fibSubList)
