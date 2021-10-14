07da242e2b1a7be9929cb1f2d1f8d1c78a595b62
chenfuyuan <chenfuyuan0713@163.com>
2021-10-14 19:15:03 +0800

    测试 文件读取/写入

---
+++ io/test_open.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :test_open.py
+# @Time      :2021/10/14 0:09
+# @Author    :Chen
+
+'测试open()函数'
+
+
+with open("test.txt",'r') as f:
+    print(f.read())
+
+f = open("test.txt",'r')
+print("================readlines==============")
+for x in f.readlines():
+    print(x.strip())
+
+print("================readByte================")
+fb = open("test_chinese.txt","rb")
+for x in fb.readlines():
+    print(x.strip())
+
+print("================readChinese=============")
+fc = open("test_chinese.txt",'r',encoding='utf-8',errors='ignore')
+for x in fc.readlines():
+    print(x.strip())
+
+fw = open("test_write.txt","a",encoding="utf-8")
+fw.write("hello world")
+fw.write("测试文档")
+fw.close()
+
+print("===")
+fc = open("test_chinese.txt",'r',encoding='utf-8',errors='ignore')
+print(fc.read(3))
