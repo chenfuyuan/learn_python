87b7ebbbcf451f5fb28d31d5217734d28d53e21c
chenfuyuan <chenfuyuan0713@163.com>
2021-10-15 20:24:47 +0800

    学习 序列化-pickle 进行反序列化

--- io/io_pickle.py
+++ io/io_pickle.py
@@ -9,5 +9,12 @@
 import pickle;
 
 d = dict(name="Bob",age=20,score=80)
+#将对象转化为bytes
+dbyte = pickle.dumps(d)
+print(dbyte)
+print(type(dbyte))
+
+#将对象保存到文件中
 f = open("pickleObj",'wb')
 pickle.dump(d,f)
+f.close()
---
+++ io/load_obj_by_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :load_obj_by_file.py
+# @Time      :2021/10/15 20:06
+# @Author    :Chen
+
+''import pickle;
+
+#从文件中 反序列化为对象
+f = open("pickleObj","rb")
+d = pickle.load(f)
+f.close()
+print(d)
+
